b77b3bb06d6431cc8f3ccb1ca441d8735da0bf2d
Nayeli Pleskac <pleskac510@gmail.com>
2021-02-15 18:58:57 -0800

    product arguments

--- lib/screens/all_listings_screen.dart
+++ lib/screens/all_listings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../widgets/drawer.dart';
 import '../widgets/all_listings_item.dart';
@@ -7,40 +8,46 @@ import '../models/product.dart';
 class AllListingsScreen extends StatelessWidget {
   static const routeName = '/all-listings';
 
-  List<Product> _items = [
-    Product(
-      id: 'p1',
-      title: 'CA T-Shirt',
-      description: 'High quality ',
-      price: 100.00,
-      imageUrl:
-          'https://curtsy-parse-files.s3-us-west-2.amazonaws.com/34f605708cd1d1638492d4d433caa906_photo.jpeg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Pants',
-      description: 'good quality',
-      price: 22.99,
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSySN9lHWMQJZZuFQF8SiQhPurCOX5RhkyzMTnDACCwpfsP-05GS5dfcLAr2PWhwZh2GVNNvnYt&usqp=CAc',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Bucket Hat',
-      description: 'Good quality for the summer',
-      price: 24.99,
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT84ImUoaVN1vBBI-GcpryJ7W2t1Q-9WhcnWw&usqp=CAU',
-    ),
-    Product(
-      id: 'p4',
-      title: 'Tank Top',
-      description: 'very soft',
-      price: 49.99,
-      imageUrl:
-          'https://media1.popsugar-assets.com/files/thumbor/ElMFl63Geo_oKFDu-IYukAf9LHc/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/07/18/631/n/1922564/36f213929fbe8c85_netimgzsJBXw/i/Shein-Crop-Cami-Top.jpg',
-    ),
-  ];
+  // List<Product> _items = [
+  //   Product(
+  //     id: 'p1',
+  //     title: 'CA T-Shirt',
+  //     description: 'High quality ',
+  //     price: 100.00,
+  //     imageUrl:
+  //         'https://curtsy-parse-files.s3-us-west-2.amazonaws.com/34f605708cd1d1638492d4d433caa906_photo.jpeg',
+  //   ),
+  //   Product(
+  //     id: 'p2',
+  //     title: 'Pants',
+  //     description: 'good quality',
+  //     price: 22.99,
+  //     imageUrl:
+  //         'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSySN9lHWMQJZZuFQF8SiQhPurCOX5RhkyzMTnDACCwpfsP-05GS5dfcLAr2PWhwZh2GVNNvnYt&usqp=CAc',
+  //   ),
+  //   Product(
+  //     id: 'p3',
+  //     title: 'Bucket Hat',
+  //     description: 'Good quality for the summer',
+  //     price: 24.99,
+  //     imageUrl:
+  //         'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT84ImUoaVN1vBBI-GcpryJ7W2t1Q-9WhcnWw&usqp=CAU',
+  //   ),
+  //   Product(
+  //     id: 'p4',
+  //     title: 'Tank Top',
+  //     description: 'very soft',
+  //     price: 49.99,
+  //     imageUrl:
+  //         'https://media1.popsugar-assets.com/files/thumbor/ElMFl63Geo_oKFDu-IYukAf9LHc/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/07/18/631/n/1922564/36f213929fbe8c85_netimgzsJBXw/i/Shein-Crop-Cami-Top.jpg',
+  //   ),
+  // ];
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  CollectionReference allListingsItem =
+      FirebaseFirestore.instance.collection('All Listings Item');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,23 +61,33 @@ class AllListingsScreen extends StatelessWidget {
         ),
       ),
       drawer: MainDrawer(),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: _firestore.collection('All Listings Item').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong...');
+            }
 
-      body: Container(
-        height: 800,
-        child: ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Container(
-              child: AllListingsItem(
-                id: _items[index].id,
-                title: _items[index].title,
-                price: _items[index].price,
-                imageUrl: _items[index].imageUrl,
-              ),
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text('Loading...');
+            }
+
+            return ListView.builder(
+              itemBuilder: (ctx, index) {
+                DocumentSnapshot allListingsDoc = snapshot.data.docs[index];
+                return Container(
+                  child: AllListingsItem(
+                    id: allListingsDoc.id,
+                    title: allListingsDoc['title'],
+                    price: allListingsDoc['price'].toDouble(),
+                    imageUrl: allListingsDoc['imageUrl'],
+                  ),
+                );
+              },
+              itemCount: snapshot.data.docs.length,
             );
-          },
-          itemCount: _items.length,
-        ),
-      ),
+          }),
     );
   }
 }
--- lib/widgets/all_listings_item.dart
+++ lib/widgets/all_listings_item.dart
@@ -4,8 +4,6 @@ import '../screens/all_listings_detail_screen.dart';
 import '../screens/all_listings_screen.dart';
 import '../models/product.dart';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class AllListingsItem extends StatelessWidget {
   final String id;
   final String title;
@@ -19,65 +17,53 @@ class AllListingsItem extends StatelessWidget {
     @required this.imageUrl,
   });
 
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  CollectionReference allListingsItem =
-        FirebaseFirestore.instance.collection('All Listings Item');
-
-  
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(30),
-          child: Container(
-            height: 300,
-            child: Card(
-              elevation: 5,
-              margin: EdgeInsets.all(20),
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-                      child: Image.network(
-                        imageUrl,
-                        fit: BoxFit.cover,
-                      ),
+        Container(
+          height: 300,
+          child: Card(
+            elevation: 5,
+            margin: EdgeInsets.all(20),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.network(
+                      imageUrl,
+                      fit: BoxFit.cover,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(title),
-                          Text('\$$price'),
-                          IconButton(
-                            icon: Icon(Icons.arrow_forward),
-                            onPressed: () {
-                              Navigator.of(context).pushNamed(
-                                AllListingsDetailScreen.routeName,
-                                arguments: Product(
-                                  id: allListingsItem.id,
-                                  title: 'title',
-                                  description:
-                                      'good quality, adkjhgbvf hjvjbf kdjhbjv fkdgcan i ne er  i lain',
-                                  price: 100,
-                                  imageUrl:
-                                      'https://curtsy-parse-files.s3-us-west-2.amazonaws.com/34f605708cd1d1638492d4d433caa906_photo.jpeg',
-                                ),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(title),
+                        Text('\$$price'),
+                        IconButton(
+                          icon: Icon(Icons.arrow_forward),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed(
+                              AllListingsDetailScreen.routeName,
+                              arguments: Product(
+                                id: id,
+                                title: title,
+                                description: 'description',
+                                price: price,
+                                imageUrl: imageUrl,
+                              ),
+                            );
+                          },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
