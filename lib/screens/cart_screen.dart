295701d29763e10b698868eaab6f19619c868e03
Nayeli Pleskac <pleskac510@gmail.com>
2021-03-07 17:59:12 -0800

    setState within cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../widgets/drawer.dart';
 import '../widgets/cart_item.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   final String id;
   final String title;
   final double price;
@@ -19,24 +19,58 @@ class CartScreen extends StatelessWidget {
     @required this.isInCart,
   });
   static const routeName = '/cart-screen';
+
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  // CollectionReference allListingsItem =
-  //     FirebaseFirestore.instance.collection('All Listings Item');
+  CollectionReference allListingsItem =
+      FirebaseFirestore.instance.collection('All Listings Item');
 
-  // Future<void> updateDoc() {
-  //   return allListingsItem
-  //       .doc('2Y9JG6ZrdZyPC4GMzQej')
-  //       .update({'isInCart': true})
-  //       .then((value) => print('cart and firebase updated'))
-  //       .catchError(
-  //           (error) => print('failed to update doc on firebase: $error'));
-  // }
+  double totalAmount = 3000;
+
+  Future<double> getTotalAmount() async {
+    var query = await allListingsItem
+        .get()
+        .then((QuerySnapshot querySnap) => querySnap);
+    //the start of the total
+    var total = 0.0;
+
+    // query.docs[i]['price'] will get the doc and the
+    //price within it
+
+    //query.size = query.docs.length
+    for (int i = 0; i < query.size; i++) {
+      total += query.docs[i]['price'];
+    }
+    return total;
+  }
+
+  @override
+
+  //setState is when the app rebuilds and it needs to be rebuilded in order to update the screen. same concept for a statefull widget
+  //will manually update the screen with the setstate to the correct value - 148 - not 3000, which is the 
+  void initState() {
+    print(
+        'LASDLJKFCLKJLFKJDFLGJKFLKJVLFKJVNJDFL;SDKFJGLKSJNKJLKDFSJ SD GSLK GSDLKFJG SDFLK;G');
+    getTotalAmount().then((totalA) {
+      setState(() {
+        print(
+          totalA,
+        );
+        print('tesing');
+        totalAmount = totalA;
+      });
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // final Product args = ModalRoute.of(context).settings.arguments;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -49,112 +83,131 @@ class CartScreen extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: MainDrawer(),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              alignment: Alignment.centerLeft,
-              child: StreamBuilder<QuerySnapshot>(
-                  stream: _firestore
-                      .collection('All Listings Item')
-                      .where('isInCart', isEqualTo: true)
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    int totalItemCount = snapshot.data.docs.length;
-                    return Text(
-                      totalItemCount.toString() + ' items',
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                    );
-                  }),
+      body: Container(
+        // color: Colors.red,
+        height: 900,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
             ),
-          ),
-          StreamBuilder<QuerySnapshot>(
-              stream: _firestore
-                  .collection('All Listings Item')
-                  .where('isInCart', isEqualTo: true)
-                  .snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Something went wrong...');
-                }
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: StreamBuilder<QuerySnapshot>(
+                    stream: _firestore
+                        .collection('All Listings Item')
+                        .where('isInCart', isEqualTo: true)
+                        .snapshots(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.data.size == 0) {
+                        print('SNAPSHOT.data.docs == null');
+                      }
+                      if (snapshot.hasError) {
+                        return Text('Something went wrong...');
+                      }
 
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Text('Loading...');
-                }
-
-                return Expanded(
-                  flex: 3,
-                  child: ListView.builder(
-                    itemBuilder: (ctx, index) {
-                      DocumentSnapshot cartDoc = snapshot.data.docs[index];
-                      return Container(
-                        child: CartItem(
-                          id: cartDoc.id,
-                          title: cartDoc['title'],
-                          // description: allListingsDoc['description'],
-                          price: cartDoc['price'].toDouble(),
-                          imageUrl: cartDoc['imageUrl'],
-                          isInCart: cartDoc['isInCart'],
-                        ),
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Text('Loading...');
+                      }
+                      int totalItemCount = snapshot.data.docs.length;
+                      return Text(
+                        totalItemCount.toString() + ' items',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
                       );
-                    },
-                    itemCount: snapshot.data.docs.length,
-                  ),
-                );
-              }),
-          Expanded(
-            flex: 2,
-            child: Column(
-              children: [
-                Card(
-                  margin: EdgeInsets.all(15),
-                  child: Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Total:',
-                          style: TextStyle(fontSize: 25),
-                        ),
-                        Spacer(),
-                        Chip(
-                          label: Text(
-                            '\$${56}',
-                            style: TextStyle(
-                              color: Theme.of(context)
-                                  .primaryTextTheme
-                                  .headline6
-                                  .color,
-                            ),
-                          ),
-                          backgroundColor: Theme.of(context).primaryColor,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 350,
-                  child: RaisedButton(
-                    color: Theme.of(context).primaryColor,
-                    onPressed: () {},
-                    child: Text(
-                      'Checkout',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  ),
-                ),
-              ],
+                    }),
+              ),
             ),
-          ),
-        ],
+            StreamBuilder<QuerySnapshot>(
+                stream: _firestore
+                    .collection('All Listings Item')
+                    .where('isInCart', isEqualTo: true)
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Something went wrong...');
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text('Loading...');
+                  }
+
+                  return Expanded(
+                    flex: 3,
+                    child: ListView.builder(
+                      itemBuilder: (ctx, index) {
+                        DocumentSnapshot cartDoc = snapshot.data.docs[index];
+                        return Container(
+                          child: CartItem(
+                            id: cartDoc.id,
+                            title: cartDoc['title'],
+                            // description: allListingsDoc['description'],
+                            price: cartDoc['price'].toDouble(),
+                            imageUrl: cartDoc['imageUrl'],
+                            isInCart: cartDoc['isInCart'],
+                          ),
+                        );
+                      },
+                      itemCount: snapshot.data.docs.length,
+                    ),
+                  );
+                }),
+            Expanded(
+              flex: 2,
+              child: Container(
+                // color: Colors.black,
+                // height: 100,
+                child: Column(
+                  children: [
+                    Card(
+                      margin: EdgeInsets.all(15),
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total:',
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            Spacer(),
+                            Chip(
+                              label: Text(
+                                '\$$totalAmount',
+                                style: TextStyle(
+                                  color: Theme.of(context)
+                                      .primaryTextTheme
+                                      .headline6
+                                      .color,
+                                ),
+                              ),
+                              backgroundColor: Theme.of(context).primaryColor,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Text(getTotalAmount().toString()),
+                    Container(
+                      width: 350,
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColor,
+                        onPressed: () {},
+                        child: Text(
+                          'Checkout',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
